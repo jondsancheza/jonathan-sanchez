52029b857f6a4abffe1b84a5c03b60f1c3917548
Jonathan Sánchez <jondsancheza@gmail.com>
2023-02-14 00:25:54 -0500

    changes src projects

--- src/Content.js
+++ src/Content.js
@@ -12,14 +12,14 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import project4 from "./assets/images/projects/img4.png";
-import project5 from "./assets/images/projects/img5.png";
-import project6 from "./assets/images/projects/img6.png";
-import project7 from "./assets/images/projects/img7.png";
-import person_project from "./assets/images/projects/person.svg";
+import project1 from "./assets/images/Projects/img1.png";
+import project2 from "./assets/images/Projects/img2.png";
+import project3 from "./assets/images/Projects/img3.png";
+import project4 from "./assets/images/Projects/img4.png";
+import project5 from "./assets/images/Projects/img5.png";
+import project6 from "./assets/images/Projects/img6.png";
+import project7 from "./assets/images/Projects/img7.png";
+import person_project from "./assets/images/Projects/person.svg";
 
 import Hireme_person from "./assets/images/Hireme/person.svg";
 
